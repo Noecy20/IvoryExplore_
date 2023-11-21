37095fa30f2e248285517d18e18cdbe2850142b4
adama12021 <adamscam925@gmail.com>
2023-11-21 16:06:53 +0000

    modification hôtel mise en place du frontend

--- app.py
+++ app.py
@@ -9,9 +9,16 @@ def bienvenue():
 @app.route("/base")
 def base():
     return render_template("base.html")
+
 @app.route("/restaurants")
 def restaurants():
     return render_template("restaurants/restaurant.html")
 
+@app.route("/hotel")
+def hotel():
+    return render_template("hotels/hotel.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
+
+ 
